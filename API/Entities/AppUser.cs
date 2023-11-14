9393231fa9b4e4a2eaf21932d4a7b4d72471b1c1
alijanaan <alijanaan@yahoo.com>
2023-11-14 16:07:48 +0500

    end of section 2

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,14 @@
+
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+
+        public String UserName { get; set; }
+        
+
+    }
+}
